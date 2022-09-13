99c487ab2945c5e23eb563b4ea6ff6a7adf87250
Mário Lucas <mariolucasdev@gmail.com>
2022-09-13 07:34:55 -0300

    Aula 02 C# - Declaração de Variáveis

---
+++ aula-02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Variaveis
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            decimal altura;
+            string nome;
+
+            nome = "Mário Lucas";
+
+            string email = "mariolucasdev@gmail.com";
+
+            Console.WriteLine(email);
+        }
+    }
+}
